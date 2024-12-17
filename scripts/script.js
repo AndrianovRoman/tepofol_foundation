b5a3a01237fe753a886348cd264ed026f32c61b7
Andrianov Roman <r_andrianov02@mail.ru>
2024-12-17 13:47:34 +0300

    fix bug in safari #1

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,57 @@
 
+$(document).ready(function() {
+    $('.material-image-items').not('.slick-initialized').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [
+            // {
+            //     breakpoint: 1025,
+            //     settings: {
+            //         slidesToShow: 3,
+            //         slidesToScroll: 1,
+            //     }
+            // },
+        ]
+    });
 
-$('.material-image-items').not('.slick-initialized').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-    responsive: [
-        // {
-        //     breakpoint: 1025,
-        //     settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 1,
-        //     }
-        // },
-    ]
-});
-
-$('.videos-content').not('.slick-initialized').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true,
-    // variableWidth: true,
-    responsive: [
-        {
-            breakpoint: 1001,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                dots: true,
-                arrows: false,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                dots: true,
-                arrows: false,
-            }
-        },
-        {
-            breakpoint: 500,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                dots: true,
-                arrows: false,
-            }
-        },
-    ]
+    $('.videos-content').not('.slick-initialized').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        // variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1001,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+        ]
+    });
 });
